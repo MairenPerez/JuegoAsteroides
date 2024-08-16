7b32be18ae890693ecae5bcec6c4f9ea5315fd83
Mairen <perez.ruiz.mairen@alumnat.copernic.cat>
2024-08-16 14:20:52 +0200

    Añadir los fotogramas por segundo

--- main/Window.java
+++ main/Window.java
@@ -5,6 +5,7 @@ import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.Color;
 
 public class Window extends JFrame implements Runnable {
     public static final int WIDTH = 800, HEIGHT = 600;
@@ -15,6 +16,12 @@ public class Window extends JFrame implements Runnable {
     // Para dibujar
     private BufferStrategy bs; 
     private Graphics g;
+
+    // Fotogramas por segundo
+    private final int FPS = 60;
+    private double TARGETTIME = 1000000000 / FPS; // 1 segundo en nanosegundos
+    private double delta = 0; // Diferencia de tiempo entre un fotograma y otro
+    private int AVERAGEFPS = FPS; // Promedio de FPS que se muestra en pantalla
 
     public Window() {
         setTitle("Juego de naves"); 
@@ -58,7 +65,10 @@ public class Window extends JFrame implements Runnable {
         // Dibujamos los elementos
 
         g.clearRect(0, 0, WIDTH, HEIGHT);
-        g.drawRect(x,0, 100, 100);
+
+        g.setColor(Color.black);
+
+        g.drawString("" + AVERAGEFPS, 10, 10);
 
         //...............................................
         g.dispose();
@@ -68,9 +78,34 @@ public class Window extends JFrame implements Runnable {
 
     @Override
     public void run() {
+
+        long now = 0; // Tiempo actual 
+        long lastTime = System.nanoTime(); // Tiempo actual del sistema en nanosegundos 
+        int frames = 0; // Contador de fotogramas
+        long time = 0; // Tiempo transcurrido
+
+
         while (running) {
-            update(); // Actualizamos los elementos
-            draw(); // Dibujamos los elementos
+            now = System.nanoTime(); // Actualizamos el tiempo actual
+            delta += (now - lastTime) / TARGETTIME; // Calculamos la diferencia de tiempo entre un fotograma y otro
+            time += (now - lastTime); // Calculamos el tiempo transcurrido
+            lastTime = now; // Actualizamos el tiempo actual
+
+            // Si la diferencia de tiempo es mayor o igual a 1
+            if (delta >= 1) {
+                update(); // Actualizamos los elementos
+                draw(); // Dibujamos los elementos
+                delta--; // Disminuimos la diferencia de tiempo
+                frames++; // Incrementamos el contador de fotogramas
+                System.out.println(frames);
+            }
+
+            if (time >= 1000000000) { // Si ha pasado un segundo ...
+                AVERAGEFPS = frames; // Actualizamos el promedio de fotogramas
+                frames = 0; // Reiniciamos el contador de fotogramas
+                time = 0; // Reiniciamos el tiempo transcurrido
+
+            }
         }
 
         stop(); // Detenemos el hilo
